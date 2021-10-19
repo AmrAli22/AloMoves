c86bc26e310dc803ad825bac208f5ed5064ec493
AmrAli22 <amr.alifciscu@gmail.com>
2021-10-19 20:13:18 +0200

    anchorView
    add cover and table constraints

--- AloMoves/TheChallange/View/ChallangeView.swift
+++ AloMoves/TheChallange/View/ChallangeView.swift
@@ -6,10 +6,13 @@
 //
 
 import UIKit
+import SnapKit
+
 class ChallangeView : UIViewController {
     
     lazy var CoverView: UIImageView = {
-        let hv = UIImageView(image: UIImage(named: ""))
+        let hv = UIImageView(image: UIImage(named: "FirstCover"))
+        hv.contentMode = .scaleAspectFill
         hv.backgroundColor = .clear
         return hv
     }()
@@ -23,10 +26,28 @@ class ChallangeView : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
         
     }
     
     private func setupUI(){
+        view.backgroundColor = .white
         view.addSubview(tableView)
+        view.addSubview(CoverView)
+        
+        CoverView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.height.equalTo(400)
+            make.centerX.equalToSuperview()
+            make.top.equalToSuperview()
+            make.bottom.equalTo(tableView.snp.top)
+        }
+        
+        tableView.snp.makeConstraints { make in
+            make.width.equalToSuperview()
+            make.centerX.equalToSuperview()
+            make.top.equalTo(CoverView.snp.bottom)
+            make.bottom.equalToSuperview()
+        }
     }
 }
